d768b180546812db5e112b3a5814201cbc397a9c
M. Shiqo Filla <shiqo@stellardata.ai>
2026-04-25 15:12:27 +0700

    chore: update version number

--- src/jsonshquery/__init__.py
+++ src/jsonshquery/__init__.py
@@ -2,7 +2,7 @@
 jsonshquery - Query JSON files using Elasticsearch Query DSL syntax
 """
 
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 __author__ = "M. Shiqo Filla"
 __email__ = "mshiqofilla18@gmail.com"
 
